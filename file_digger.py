2188a1c8c240bfbf73b736633881b14aaa27ab9b
mengfeijin <jin.xzy@outlook.com>
2025-08-31 11:27:28 -0700

    Some formating.

--- file_digger.py
+++ file_digger.py
@@ -1,36 +1,42 @@
 import os
 import sys
 
-def main():
-    directory = input("Enter the full path to root folder: ")
+def get_root_directory() -> str:
+    return input("Enter the full path to the root folder: ")
 
-    filename = "python_files_list.txt"
-    folders_to_ignore = {'venv', '.venv', '__pycache__'}
-    output_path = os.path.join(directory, filename)
 
-    if os.path.exists(output_path):
-        print(f"File '{output_path}' already exists, do you want to replace it? (y/n)")
+def check_file_handle_exceptions(file_full_path: str) -> None:
+    if os.path.exists(file_full_path):
+        print(f"File '{file_full_path}' already exists, do you want to replace it? (y/n)")
         choice = input().lower()
+
         if choice == "y":
             try:
-                os.remove(output_path)
-                print(f"Old file '{output_path}' removed.")
+                os.remove(file_full_path)
+                print(f"Old file '{file_full_path}' removed.")
             except OSError as e:
                 print(f"Error removing file: {e}")
                 sys.exit(1)
+
         elif choice == "n":
             print("Operation cancelled. Program terminated.")
             sys.exit(0)
+
         else:
             print("Unknown option. Program terminated.")
             sys.exit(1)
 
+
+def write_file(directory: str, file_full_path: str) -> None:
+    folders_to_ignore = {'venv', '.venv', '__pycache__'}
+
     try:
-        with open(output_path, "w") as f:
+        with open(file_full_path, "w") as f:
             f.write(f"Python files found in: {directory}\n\n")
-            for dirpath, dirnames, filenames in os.walk(directory):
-                #Ignore the unwanted folders.
-                dirnames[:] = [name for name in dirnames if name not in folders_to_ignore and not name.startswith('.')]
+
+            for dirpath_to_current_folder, dir_names_in_current_folder, filenames in os.walk(directory):
+                # This line modifies dirnames_in_current_folder in place, preventing os.walk to enter unwanted directories.
+                dir_names_in_current_folder[:] = [name for name in dir_names_in_current_folder if name not in folders_to_ignore and not name.startswith('.')]
                 for filename in filenames:
                     if filename.endswith(".py"):
                         f.write(f'{filename}\n')
@@ -40,6 +46,16 @@ def main():
     except IOError as e:
         print(f"Error: Could not write to file. {e}")
         sys.exit(1)
+
+
+def main():
+    directory = get_root_directory()
+
+    filename = "python_files_list.txt"
+    file_full_path = os.path.join(directory, filename)
+
+    check_file_handle_exceptions(file_full_path)
+    write_file(directory, file_full_path)
 
 
 if __name__ == "__main__":
