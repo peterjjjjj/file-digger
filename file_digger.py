ae90041527eb35f66be398f5258e8779a916b2cc
mengfeijin <jin.xzy@outlook.com>
2025-08-31 02:40:40 -0700

    Update filter condition.

--- file_digger.py
+++ file_digger.py
@@ -5,6 +5,7 @@ def main():
     directory = input("Enter the full path to root folder: ")
 
     filename = "python_files_list.txt"
+    folders_to_ignore = {'venv', '.venv', '__pycache__'}
     output_path = os.path.join(directory, filename)
 
     if os.path.exists(output_path):
@@ -27,7 +28,9 @@ def main():
     try:
         with open(output_path, "w") as f:
             f.write(f"Python files found in: {directory}\n\n")
-            for dirpath, _, filenames in os.walk(directory):
+            for dirpath, dirnames, filenames in os.walk(directory):
+                #Ignore the unwanted folders.
+                dirnames[:] = [name for name in dirnames if name not in folders_to_ignore]
                 for filename in filenames:
                     if filename.endswith(".py"):
                         f.write(f'{filename}\n')
